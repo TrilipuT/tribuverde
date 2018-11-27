caa260571332d137320e76732fc25558be2d9eba
Vitaly Nikolaiev <trilliput@gmail.com>
2018-11-28 00:08:23 +0200

    animations

--- assets/src/javascripts/common.js
+++ assets/src/javascripts/common.js
@@ -1,5 +1,10 @@
 import $ from 'jquery'
 import AOS from 'aos'
+import SVG from 'svg.js'
+
+function getRand (min, max) {
+  return Math.random() * (max - min) + min
+}
 
 $(function ($) {
   AOS.init({
@@ -16,6 +21,56 @@ $(function ($) {
     } else {
       $topPart.removeClass('to-left')
     }
+  }
+  if ($('body').hasClass('home')) {
+    setInterval(function () {
+      //$topPart.toggleClass('to-left')
+    }, 20000)
+  }
 
+  // Animations
+  function arrowsAnimation () {
+    arrows.animate(3000).
+      dmove(80, 0).
+      loop(1000, true)
+  }
+
+  function homeAnimation (element) {
+    let r = Math.random()
+    element.animate(4000).
+      dmove(15 * r, 15 * r).
+      animate(4000).
+      dmove(-10 * r, -5 * r).
+      animate(4000).
+      dmove(-10 * r, -5 * r).
+      animate(4000).
+      dmove(5 * r, -5 * r).
+      after(function () {
+        homeAnimation(element)
+      })
+  }
+
+  if ($('#front-page').length) {
+    var front = SVG.get('#front-page'),
+      projects = front.select('#home-projects'),
+      shop = front.select('#home-shop'),
+      about = front.select('#home-about'),
+      films = front.select('#home-films'),
+      arrows = front.select('#Arrows')
+
+    arrowsAnimation()
+    homeAnimation(about)
+    homeAnimation(films)
+    homeAnimation(projects)
+    homeAnimation(shop)
+
+    about.mouseover(function () { this.pause() })
+    shop.mouseover(function () { this.pause() })
+    films.mouseover(function () { this.pause() })
+    projects.mouseover(function () { this.pause() })
+    about.mouseout(function () { this.play() })
+    shop.mouseout(function () { this.play() })
+    films.mouseout(function () { this.play() })
+    projects.mouseout(function () { this.play() })
   }
 })
--- header.php
+++ header.php
@@ -23,8 +23,8 @@
 				'container'       => 'nav',
 				'container_class' => 'menu-content',
 				'menu_class'      => 'menu-items-content',
-					'link_before'     => '<span data-aos="fade-right">',
-			'link_after'      => '</span>',
+				'link_before'     => '<span data-aos="fade-right">',
+				'link_after'      => '</span>',
 			] ) ?>
         </div>
 
@@ -32,14 +32,28 @@
 <?php endif; ?>
 <div class="background">
     <div class="forest">
-         <img src="<?= get_template_directory_uri() ?>/assets/built/images/forest.jpg" alt="">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="600 550 0 0" width="600" height="300">
-            <defs>
-                <clipPath id="forest-mask">
-                    <path d="M62.27 336.44c44.6 33.56 113.71 20.67 156.79-13.96 22.14-17.91 28.3-44.73 29.26-71.98.9-17.63-3.03-31.28-12.97-46.16-7.55-10.86-13.51-20.11-15.84-33.75-3.1-19.26 1.81-37.67 11.92-54.04 7.28-11.98 14.96-23.56 16.25-38.39 5.91-52.87-69.17-101.38-111.86-64.75-22.95 19.51-41.14 47.06-44.51 76.7-2.94 23.63-10.62 35.21-29.13 48.73-19.31 13.52-39.41 23.83-51.16 45.8-10.93 19.58-29.76 90.81 51.26 151.78z"/>
-                </clipPath>
-            </defs>
-        </svg>
+<!--        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">-->
+<!--            <defs>-->
+<!--                <clipPath id="mask">-->
+<!--                    <path d="M62.27 336.44c44.6 33.56 113.71 20.67 156.79-13.96 22.14-17.91 28.3-44.73 29.26-71.98.9-17.63-3.03-31.28-12.97-46.16-7.55-10.86-13.51-20.11-15.84-33.75-3.1-19.26 1.81-37.67 11.92-54.04 7.28-11.98 14.96-23.56 16.25-38.39 5.91-52.87-69.17-101.38-111.86-64.75-22.95 19.51-41.14 47.06-44.51 76.7-2.94 23.63-10.62 35.21-29.13 48.73-19.31 13.52-39.41 23.83-51.16 45.8-10.93 19.58-29.76 90.81 51.26 151.78z"/>-->
+<!--                </clipPath>-->
+<!--            </defs>-->
+<!--            <g clip-path="url(#mask)">-->
+<!--                <image xmlns:xlink="http://www.w3.org/1999/xlink"-->
+<!--                       xlink:href="--><?//= get_template_directory_uri() ?><!--/assets/built/images/forest.jpg" width="1120px"-->
+<!--                       height="840px"></image>-->
+<!--            </g>-->
+<!--        </svg>-->
+
+
+        <!--         <img src="--><? //= get_template_directory_uri() ?><!--/assets/built/images/forest.jpg" alt="">-->
+        <!--        <svg xmlns="http://www.w3.org/2000/svg" viewBox="600 550 0 0" width="600" height="300">-->
+        <!--            <defs>-->
+        <!--                <clipPath id="forest-mask">-->
+        <!--                    <path d="M62.27 336.44c44.6 33.56 113.71 20.67 156.79-13.96 22.14-17.91 28.3-44.73 29.26-71.98.9-17.63-3.03-31.28-12.97-46.16-7.55-10.86-13.51-20.11-15.84-33.75-3.1-19.26 1.81-37.67 11.92-54.04 7.28-11.98 14.96-23.56 16.25-38.39 5.91-52.87-69.17-101.38-111.86-64.75-22.95 19.51-41.14 47.06-44.51 76.7-2.94 23.63-10.62 35.21-29.13 48.73-19.31 13.52-39.41 23.83-51.16 45.8-10.93 19.58-29.76 90.81 51.26 151.78z"/>-->
+        <!--                </clipPath>-->
+        <!--            </defs>-->
+        <!--        </svg>-->
     </div>
     <div class="logo-animation">
         <span class="top-part" data-aos="fade-top"
